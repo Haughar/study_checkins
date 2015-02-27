3fb4547af594fc54b994954468b38d9e86056282
haughar <haughar@uw.edu>
2015-02-26 17:11:10 -0800

    Change the jQuery to be connected over https instead of http.

--- common.php
+++ common.php
@@ -10,7 +10,7 @@
 			<meta charset="utf-8" />
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-			<script src="http://code.jquery.com/jquery.js"></script>
+			<script src="https://code.jquery.com/jquery.js"></script>
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 			<link rel="stylesheet" type="text/css" href="css/main.css">
 			<script type="text/javascript">
